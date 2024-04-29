47b0d95104e012c5674190c3057e5e3f17af3409
Aidan <aidanchiu1112@gmail.com>
2024-04-29 12:08:33 -0700

    test: added things to readerjava

--- src/Reader.java
+++ src/Reader.java
@@ -1,6 +1,7 @@
 public class Reader {
     public Reader(String imageFileName, String labelFileName) {
         System.out.println("hi aidan");
+        System.out.println("yessir");
     }
 
     public Image[] read() {
