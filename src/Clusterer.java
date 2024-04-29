5fc3dfb0c1c04214839d690b8e60abeb699aacc6
jackhughes <jhughes24@priorypanther.com>
2024-04-29 14:53:24 -0700

    Merge remote-tracking branch 'origin/main'

--- src/Clusterer.java
+++ src/Clusterer.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Clusterer {
+
+    public static void main(String[] args) {
+        Image[] images = new Image[1000];
+        for (int i = 0; i < images.length; i++) {
+            images[i] = new Image(new byte[3][3]);
+        }
+
+        Clusterer clusterer = new Clusterer();
+
+        clusterer.cluster(images, 10);
+    }
 
     // takes in data and creates labels from it
 
@@ -11,7 +25,29 @@ public class Clusterer {
     // continue until "stabilized" -> very few images move buckets each iteration
 
     public int[] cluster(Image[] images) {
+        return cluster(images, Math.min(10, images.length));
+    }
+    public int[] cluster(Image[] images, int k) {
+        assert images.length >= k;
+
+        // bucket array cannot be an array; "You cannot create arrays of parameterized types"
+        ArrayList<Image>[] buckets = createBuckets(k);
+
+        for (int i = 0; i < k; i++) {
+            buckets[i].add(images[i]);
+        }
+
         return null;
+    }
+
+    public ArrayList<Image>[] createBuckets(int k) {
+        ArrayList<Image>[] buckets = new ArrayList[k];
+
+        for (int i = 0; i < k; i++) {
+            buckets[i] = new ArrayList<Image>();
+        }
+
+        return buckets;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,5 @@ import java.io.InputStream;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        Reader reader = new Reader();
-        Image[] images = reader.read();
-    }
+    public static void main(String[] args) {}
 }
