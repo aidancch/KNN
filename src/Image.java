86ca8d0531a26a044fcf4fe1798d8e1a96f2220f
jackhughes <jhughes24@priorypanther.com>
2024-05-01 09:49:32 -0700

    created TestSet class to create random test images until the reader is completed.

--- src/Image.java
+++ src/Image.java
@@ -126,7 +126,24 @@ public class Image {
     }
 
     public void downsize(int scale) {
+        assert this.rows % scale == 0 && this.columns % scale == 0;
 
+        byte[][] newPix = new byte[this.rows / scale][this.columns / scale];
+        for (int i = 0; i < newPix.length; i++) {
+            for (int j = 0; j < newPix[0].length; j++) {
+                int sum = 0;
+                for (int y = i * scale; y < (i + 1) * scale; y++) {
+                    for (int x = j * scale; x < (j + 1) * scale; x++) {
+                        sum += this.pixels[y][x];
+                    }
+                }
+                newPix[i][j] = (byte)(sum / (scale * scale));
+            }
+        }
+
+        this.pixels = newPix;
+        this.columns = this.columns / scale;
+        this.rows = this.rows / scale;
     }
 
 
